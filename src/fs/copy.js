55b88a4211a7b941ae8655f852d7d2e1d75b722c
Neiman22 <pushkarev_av@inbox.ru>
2024-01-19 15:47:54 +0300

    fix: change copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { cp } from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const src = path.join(__dirname, 'files');
+  const dest = path.join(__dirname, 'files_copy');
+
+  try {
+    await cp(src, dest, { recursive: true, force: false, errorOnExist: true });
+    console.log('Folder copied');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.error(`FS operation failed: no such directory`);
+    } if (err.code === 'ERR_FS_CP_EEXIST') {
+      console.error(`FS operation failed: folder already exists`);
+    } else {
+      throw err;
+    }
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,18 +1,17 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { open, writeFile } from 'node:fs/promises';
+import fsPromises from 'fs/promises';
 
 const create = async () => {
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
   const filePath = path.join(__dirname, 'files', 'fresh.txt');
 
   try {
-    await open(filePath, 'wx');
-    await writeFile(filePath, 'I am fresh and young');
-    console.log('File fresh.txt created');
+    await fsPromises.writeFile(filePath, 'I am fresh and young', {flag: 'wx'});
+    console.log('File "fresh.txt" created');
   } catch (err) {
     if (err.code === 'EEXIST') {
-      console.error('FS operation failed: file already exists');
+      console.error(`FS operation failed: file already exists`);
     } else {
       throw err;
     }
