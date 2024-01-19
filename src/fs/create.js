ae8b799f7c9e9f9cc90bad60e804da3678774ef0
Neiman22 <pushkarev_av@inbox.ru>
2024-01-19 10:26:01 +0300

    fix: change create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { open, writeFile } from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
+  try {
+    await open(filePath, 'wx');
+    await writeFile(filePath, 'I am fresh and young');
+    console.log('File fresh.txt created');
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      console.error('FS operation failed: file already exists');
+    } else {
+      throw err;
+    }
+  }
 };
 
 await create();
