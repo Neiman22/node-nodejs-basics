32e83eac29c0cc548c8cd6a1f0618c9edac5222d
Neiman22 <pushkarev_av@inbox.ru>
2024-01-22 14:40:52 +0300

    fix: change env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
+import process from 'process';
+
 const parseEnv = () => {
-    // Write your code here 
+  const arrayEnv = Object.entries(process.env);
+  const filterKeys = arrayEnv.filter((item) => item[0].startsWith('RSS_'));
+  const formatedKeys = filterKeys.map((item) => `${item[0]}=${item[1]}`).join('; ');
+  console.log(formatedKeys);
 };
 
 parseEnv();
