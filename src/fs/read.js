b3bceb82fe7706b83e887aab51a99b15fb276e2b
Neiman22 <pushkarev_av@inbox.ru>
2024-01-22 11:59:50 +0300

    fix: change read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fsPromises from 'fs/promises';
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const fileName = path.join(__dirname, 'files', 'fileToRead.txt');
+  
+  try {
+    const content = await fsPromises.readFile(fileName, { encoding: 'utf8' });
+    console.log('Content in "fileToRead.txt" file:');
+    console.log(content);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed: "fileToRead.txt" file does not exist');
+    } else {
+      console.error(err.message);
+    };
+  } 
 };
 
 await read();
