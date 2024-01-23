72404f605e95e3eae1893cad9ebc11f23d0e75d4
Neiman22 <pushkarev_av@inbox.ru>
2024-01-23 16:51:54 +0300

    fix: change decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { dirname, join} from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream }from 'fs'
+import { pipeline } from 'stream';
+import { createUnzip } from 'zlib';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, 'files', 'fileToCompress.txt');
+const zibPath = join(__dirname, 'files', 'archive.gz');
+
 const decompress = async () => {
-    // Write your code here 
+  const readStream = createReadStream(zibPath);
+  const writeStream = createWriteStream(filePath);
+  const gzip = createUnzip();
+
+  pipeline(readStream, gzip, writeStream, (err) => {
+    if (err) {
+      throw new Error(err.message);
+    }
+  });
 };
 
 await decompress();
