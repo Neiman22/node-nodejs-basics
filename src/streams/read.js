e5cf2a3219d5062181f25a88a7619c6f3822d04a
Neiman22 <pushkarev_av@inbox.ru>
2024-01-23 14:46:12 +0300

    fix: change read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { createReadStream } from 'fs';
+import { dirname, join} from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  const stream = createReadStream(filePath);
+  stream.pipe(process.stdout);
 };
 
 await read();
