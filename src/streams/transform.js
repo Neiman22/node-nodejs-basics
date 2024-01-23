c71447a337567842f3fff6a54d60e0a34ab3baec
Neiman22 <pushkarev_av@inbox.ru>
2024-01-23 16:14:18 +0300

    fix: change transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
-const transform = async () => {
-    // Write your code here 
+import { stdin, stdout } from 'process';
+import { Transform } from 'stream';
+
+const transform = async () => {  
+  const myTransform = new Transform({
+    transform(chunk, _, callback) {
+      this.push(chunk.toString().split("").reverse().join("") + "\n");
+      callback();
+    } 
+  });
+
+  stdin.pipe(myTransform).pipe(stdout);
 };
 
 await transform();
